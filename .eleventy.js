b871e46bb68759462fa01e6e3545a6b03bffa6d7
OTC <otc@obeytheclaw.com>
2026-02-20 23:21:43 -0500

    Initial OTC site with 11ty

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  // Copy assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+  
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
